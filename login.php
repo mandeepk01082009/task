95d2d535c7bfbd8c1e03735935ee5fa569604e9d
Mandeep Kaur <mandeepk01082009@gmail.com>
2023-02-06 14:36:17 +0530

    Add login.php

---
+++ login.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>LOGIN</title>
+</head>
+<body>
+<p>This is login page.</p>
+</body>
+</html>
